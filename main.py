48790aa75c681f762577a994a095c78994acf396
jobet1995 <87087925+jobet1995@users.noreply.github.com>
2023-10-21 04:34:50 +0000

    added POST API

--- main.py
+++ main.py
@@ -2,19 +2,43 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        try:
-            with open('templates/index.html', 'r') as f:
-                content = f.read()
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(bytes(content, 'utf8'))
-        except:
-            self.send_response(404)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            self.wfile.write(b'404: File not found')
-      
+      try:
+        if self.path == '/':
+          self.send_response(200)
+          self.send_header('Content-type', 'text/html')
+          self.end_headers()
+          response_message = "Response GET Request"
+          self.wfile.write(response_message.encode(encoding='utf-8'))
+            
+        else:
+          self.send_response(400)
+          self.send_header('Content-type','text/plain')
+          self.end_headers()
+          response_message = "Not Found"
+          self.wfile.write(response_message.encode(encoding='utf-8'))
+          
+      except Exception as e:
+          self.send_response(500)
+          self.send_header('Content-type', 'text/plain')
+          self.end_headers()
+          error_message = f"Error: {str(e)}"
+          self.wfile.write(error_message.encode('utf-8'))
+            
+    def do_POST(self):
+      try:
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        print(post_data)
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(bytes(post_data, 'utf8'))
+      except Exception as e:
+        self.send_response(500)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        error_message = f"error: {str(e)}"
+        self.wfile.write(bytes(error_message, 'utf8'))
 
-httpd = HTTPServer(('', 8080), MyHandler)
+httpd = HTTPServer(('', 8000), MyHandler)
 httpd.serve_forever()
