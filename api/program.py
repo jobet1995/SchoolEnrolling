ce6796ac77df06a90150f5098cf08e5897925f8d
jobet1995 <87087925+jobet1995@users.noreply.github.com>
2023-10-28 07:01:11 +0000

    added updates

--- api/program.py
+++ api/program.py
@@ -1,112 +1,90 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import sqlite3
 import json
+import urllib.parse
 
 class ProgramRecordHandler(BaseHTTPRequestHandler):
 
-  def send_json_response(self, response_data, status_code=200):
-    self.send_response(status_code)
-    self.send_header('Content-type', 'application/json')
-    self.end_headers()
-    self.wfile.write(json.dumps(response_data).encode())
-
-  def do_GET(self):
-    try:
-      if self.path == '/':
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+    def send_json_response(self, response_data, status_code=200):
+        self.send_response(status_code)
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
+        self.wfile.write(json.dumps(response_data).encode())
 
-        with open('templates/program.html', 'rb') as html_file:
-          response_message = html_file.read()
-        self.wfile.write(response_message)
+    def do_GET(self):
+        try:
+            if self.path == '/':
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
 
-      elif self.path == '/database':
-        conn = sqlite3.connect('database.sqlite')
-        con = conn.cursor()
-        con.execute('SELECT * FROM programs_courses')
-        database = con.fetchall()
-        conn.close()
-        self.send_json_response({'database' : database})
+                with open('templates/program.html', 'rb') as html_file:
+                    response_message = html_file.read()
+                self.wfile.write(response_message)
 
-      else:
-        self.send_response(404)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        response_message = "Not Found"
-        self.wfile.write(response_message('utf-8'))
-        self.send_json_response({'error': 'Not Found'}, status_code=404)
+            elif self.path == '/database':
+                conn = sqlite3.connect('database.sqlite')
+                con = conn.cursor()
+                con.execute('SELECT * FROM programs_courses')
+                database = con.fetchall()
+                conn.close()
+                self.send_json_response({'database': database})
 
-    except Exception as e:
-      self.send_json_response({'error': str(e)}, status_code=500)
-      self.send_response(500)
-      self.send_header('Content-type', 'text/plain')
-      self.end_headers()
-      error_message = f"Error: {str(e)}"
-      self.wfile.write(error_message.encode('utf-8'))
+            else:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                response_message = "Not Found"
+                self.wfile.write(response_message.encode('utf-8'))
+                self.send_json_response({'error': 'Not Found'}, status_code=404)
 
-  def do_POST(self):
-    try:
-      content_length = int(self.headers['Content-Length'])
-      body = self.rfile.read(content_length).decode('utf-8')
+        except Exception as e:
+            self.send_json_response({'error': str(e)}, status_code=500)
+            self.send_response(500)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            error_message = f"Error: {str(e)}"
+            self.wfile.write(error_message.encode('utf-8')
 
-      conn = sqlite3.connect('database.py')
-      cursor = conn.cursor()
-      cursor.execute('INSERT INTO programs_courses(program_name, application_deadline, available_slots, program_requirements)VALUES(?,?,?,?)', (body,))
-      conn.commit()
-      conn.close()
-      self.send_json_response({'message' : 'Received POST Data and Inserted into the database.'})
+    def do_POST(self):
+        try:
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length).decode('utf-8')
 
-      self.send_response(200)
-      self.send_header('Content-type', 'text/plain')
-      self.end_headers()
-      response_message = "Received POST data and inserted into the database."
-      self.wfile.write(response_message.encode('utf-8'))
+            conn = sqlite3.connect('database.sqlite')
+            cursor = conn.cursor()
+            data = urllib.parse.parse_qs(body)
+            program_name = data.get('program_name', [''])[0]
+            application_deadline = data.get('application_deadline', [''])[0]
+            available_slots = data.get('available_slots', [''])[0]
+            program_requirements = data.get('program_requirements', [''])[0]
 
-    except Exception as e:
-      self.send_json_response({'error': str(e)}, status_code=500)
-      self.send_response(500)
-      self.send_header('Content-type', 'text/html')
-      self.end_headers()
-      error_message = f"Error: {str(e)}"
-      self.wfile.write(bytes(error_message, 'utf8'))
-      self.send_json_response({'error': str(e)}, status_code=500)
+            cursor.execute('INSERT INTO programs_courses(program_name, application_deadline, available_slots, program_requirements) VALUES (?, ?, ?, ?)',
+                           (program_name, application_deadline, available_slots, program_requirements))
+            conn.commit()
+            conn.close()
+            self.send_json_response({'message': 'Received POST data and inserted into the database.'})
 
-  def do_PUT(self):
-    try:
-      content_length = int(self.headers.get('Content-Length'))
-      body = self.rfile.read(content_length).decode('utf-8')
-      id = int(self.path.split('/')[-1])
-      
-      conn = sqlite3.connect('database.py')
-      cursor = conn.cursor()
-      cursor.execute('UPDATE programs_courses SET program_name=?, application_deadline=?, available_slots=?, program_requirements=? WHERE id=?', (body, id))
-      conn.commit()
-      conn.close()
-      self.send_json_response({'message': f'Updated student record with ID: {id} in the database.'})
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            response_message = "Received POST data and inserted into the database."
+            self.wfile.write(response_message.encode('utf-8'))
 
-      self.send_response(200)
-      self.send_header('Content-type','text/plain')
-      self.end_headers()
-      response_message = f"Updated student record with ID: {id} in the database"
-      self.wfile.write(response_message.encode('utf-8'))
-
-    except Exception as e:
-      self.send_json_response({'error': str(e)}, status_code=500)
-      self.send_response(500)
-      self.send_header('Content-type', 'text/plain')
-      self.end_headers()
-      error_message = f"Error: {str(e)}"
-      self.wfile.write(error_message.encode('utf-8'))
-
+        except Exception as e:
+            self.send_json_response({'error': str(e)}, status_code=500)
+            self.send_response(500)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            error_message = f"Error: {str(e)}"
+            self.wfile.write(bytes(error_message, 'utf8'))
+            self.send_json_response({'error': str(e)}, status_code=500)
 
 def run(server_class=HTTPServer, handler_class=ProgramRecordHandler, port=8080):
-  server_address = ('', port)
-  httpd = server_class(server_address, handler_class)
-  print(f'Starting httpd on port {port}...\n')
-  httpd.serve_forever()
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd on port {port}...\n')
+    httpd.serve_forever()
 
 if __name__ == '__main__':
-  run()
-
-
+    run()
--- api/programTest.py
+++ api/programTest.py
@@ -6,15 +6,19 @@ class TestProgramRecordHandler(unittest.TestCase):
   def test_do_GET(self):
     response = requests.get('http://localhost:8080/')
     self.assertEqual(response.status_code, 200)
+    print("GET / - Success")
 
     response = requests.get('http://localhost:8080/database')
     self.assertEqual(response.status_code, 200)
     self.assertTrue('database' in response.json())
-
+    print("GET /database - Success")
+    
     response = requests.get('http://localhost:8080/nonexistent')
     self.assertEqual(response.status_code, 404)
     self.assertTrue('error' in response.json())
+    print("GET /nonexistent - Error: Not Found")
 
+  
   def test_do_POST(self):
     data = {
       'program_name': 'Program A',
@@ -25,6 +29,7 @@ class TestProgramRecordHandler(unittest.TestCase):
 
     response = requests.post('htt://localhost:8080/database', json=data)
     self.assertEqual(response.status_code, 200)
+    print("POST /database - Success")
 
   def test_do_PUT(self):
     data = {
@@ -36,6 +41,7 @@ class TestProgramRecordHandler(unittest.TestCase):
 
     response = requests.put('http://localhost:8080/database/Program A', json=data)
     self.assertEqual(response.status_code, 200)
+    print("PUT /database/1 - Success")
 
 if __name__ == '__main__':
   unittest.main()
